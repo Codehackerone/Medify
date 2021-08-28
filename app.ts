385edae854decce74bef43142755a6f6e2bd80b8
Soumyajit Datta <soumyajitdatta123@gmail.com>
2021-08-28 12:10:54 +0530

    files initiated

--- app.ts
+++ app.ts
@@ -1,14 +1,13 @@
 import express from 'express';
 import { config } from 'dotenv';
+import { messager } from './helpers/message';
 
 config();
 const app = express();
 const PORT= process.env.PORT;
 
-app.get('/', (req, res) => {
-    res.json({
-        'message': 'Hello World!'
-    });
+app.get('/', (req:any, res:any) => {
+    messager(res,200,'Welcome to Medify API');
 });
 
 app.listen(PORT, () => {
