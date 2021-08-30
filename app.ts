1af1d1ab1894274e0b24c7f35a40485272b3480f
Soumyajit Datta <soumyajitdatta123@gmail.com>
2021-08-30 08:39:47 +0530

    mongodb connected

--- app.ts
+++ app.ts
@@ -1,15 +1,32 @@
 import express from 'express';
 import { config } from 'dotenv';
 import { messager } from './helpers/message';
+import mongoose from 'mongoose';
 
 config();
 const app = express();
 const PORT= process.env.PORT;
+const MONGODB_URI:any= String(process.env.MONGO_URI);
+const connectOptions:any = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+    };
+
+try{
+    mongoose.connect(MONGODB_URI, connectOptions)
+}
+catch(err){
+    console.log(err);
+}
+
+mongoose.connection.once("open", () => {
+console.log("MongoDB connected");
+});
 
 app.get('/', (req:any, res:any) => {
     messager(res,200,'Welcome to Medify API');
 });
 
 app.listen(PORT, () => {
-    console.log(`Medify Server listening on ${PORT}. http://localhost:${PORT}`);
+    console.log(`Medify Server listening on ${PORT}. URL:http://localhost:${PORT}`);
 });
