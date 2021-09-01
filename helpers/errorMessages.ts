69e09ac4890cab2cb86be1505787dbec10633aa0
Soumyajit Datta <soumyajitdatta123@gmail.com>
2021-09-01 09:26:58 +0530

    error messages added

---
+++ helpers/errorMessages.ts
@@ -0,0 +1,34 @@
+export const BAD_REQUEST:any={
+    "code": 400,
+    "errorName": "Bad Request. Please check your request."
+}
+
+export const UNAUTHORIZED:any={
+    "code": 401,
+    "errorName": "Unauthorized. Please check your credentials."
+}
+
+export const FORBIDDEN:any={
+    "code": 403,
+    "errorName": "Forbidden. You are not allowed to access this resource."
+}
+
+export const NOT_FOUND:any={
+    "code": 404,
+    "errorName": "Not Found. The resource you are looking for is not found."
+}
+
+export const CONFLICT:any={
+    "code": 409,
+    "errorName": "Conflict. The resource you are trying to access is already in use."
+}
+
+export const SERVER_ERROR:any={
+    "code": 500,
+    "errorName": "Internal Server Error. Please try again later."
+}
+
+export const BAD_GATEWAY:any={
+    "code": 502,
+    "errorName": "Bad Gateway. Please try again later."
+}
