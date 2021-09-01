df415bfcd713d4ce925793452e7be1bbc4eeeb05
Soumyajit Datta <soumyajitdatta123@gmail.com>
2021-09-01 08:30:56 +0530

    login function added in controller

--- users/users.controller.ts
+++ users/users.controller.ts
@@ -12,14 +12,15 @@ const jwt_headers:any = {
 export const register = async (req:any, res:any) => {
     try {
       var user:any = await registerService(req.body);
-      var return_object = {
-        user: user,
-      };
       const access_token = jwt.sign(
         { email: user.email, user_id: user._id },
         String(process.env.JWT_SECRET),
         jwt_headers
       );
+      var return_object:any = {
+        user: user,
+        auth_token: access_token,
+      };
       messageCustom(res, 200, "user created successfully", return_object);
     } catch (err:any) {
       if (err.error === "ValidationError") {
@@ -40,3 +41,52 @@ export const register = async (req:any, res:any) => {
       }
     }
   };
+
+  export const login = async (req:any, res:any) => {
+    try {
+      if (!req.body.email || !req.body.password) {
+        var err = {
+          s_code: 400,
+          type: "ValidationError",
+          name: "Both 'email' and 'password' are required",
+        };
+        throw err;
+      }
+      var email = req.body.email;
+      var password = req.body.password;
+      const user:any = await findUserService({ email });
+      if (!user) {
+        var err = {
+          s_code: 500,
+          type: "AuthenticationError",
+          name: "Email or Password doesn't match.",
+        };
+        throw err;
+      }
+      if (!bcrypt.compareSync(password, user.password)) {
+        var err = {
+          s_code: 500,
+          type: "AuthenticationError",
+          name: "Email or Password doesn't match.",
+        };
+        throw err;
+      }
+      const access_token = jwt.sign(
+        { email: user.email, user_id: user._id },
+        String(process.env.JWT_SECRET),
+        jwt_headers
+      );
+      var return_object:any = {};
+      return_object.auth_token = access_token;
+      return_object.user = user;
+      messageCustom(res, 200, "successfully logged in", return_object);
+    } catch (err:any) {
+      if (err.s_code !== undefined) {
+        messageError(res, err.s_code, err.name, err.type);
+      } else {
+        console.log(err);
+        messageError(res, 500, "error", err);
+      }
+    }
+  };
+  
