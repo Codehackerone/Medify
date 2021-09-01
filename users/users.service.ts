13ebdfdce72501eb435f1bfd62813381066e0f0f
Soumyajit Datta <soumyajitdatta123@gmail.com>
2021-09-01 08:17:28 +0530

    user service updated

--- users/users.service.ts
+++ users/users.service.ts
@@ -1,12 +1,11 @@
 import User from './users.model';
 
-export const register = async (req:any, res:any) => {
-    const user = new User(req.body);
-    try {
-        await user.save();
-        const token = await user
-        res.status(201).send({ user, token });
-    } catch (error) {
-        res.status(400).send(error);
-    }
+export const register = async (userBody:any) => {
+    const user = await User.create(userBody);
+    return user;
 }
+
+export const findUser = async (param:any) => {
+    var user = await User.findOne(param);
+    return user;
+};
