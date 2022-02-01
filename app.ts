bc28f773c57945e9b8fc60cad60901d5fe3f2c03
sbk2k1 <bhattacharyasaptarshi2001@gmail.com>
2022-02-01 22:04:07 +0530

    Morgan Logger Added

--- app.ts
+++ app.ts
@@ -7,6 +7,12 @@ import mongoose from "mongoose";
 import userRouter from "./users/users.route";
 import routineRouter from "./routines/routine.route";
 
+//All necessary imports for logger
+import morgan from 'morgan';
+import fs from "fs";
+import path from "path";
+//
+
 config();
 
 const app = express();
@@ -27,9 +33,21 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+//logger
+//
+//setting up the output stream
+let logStream = fs.createWriteStream(path.join(__dirname, 'logFile.log'), { flags: 'a' })
+//
+// setup the logger
+app.use(morgan('combined', { stream: logStream }))
+//
+
+
 mongoose.connection.once("open", () => {
   console.log("MongoDB connected");
 });
+
 
 app.get("/", (req: any, res: any) => {
   message(res, OK, "Welcome to Medify API");
